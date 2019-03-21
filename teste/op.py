0b1bb7323b85f6a94a8348b0a07df8cec7bc92c0
Your Name <you@example.com>
2019-03-21 09:27:12 -0300

    k

--- teste/op.py
+++ teste/op.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask (__name__)
 from opa import Pessoa
 
 @app.route ("/")
 def trelar ():
     return render_template ("inicio.html")
+
+@app.route ("/add_pessoa")
+def add ():
+    nome = request.args.get("nome")
+    endereco = request.args.get ("endereco")
+    telefone = request.args.get ("telefone")
+    r = "os dados recebidos foram"
+    r += nome + "," + endereco + "," + telefone
+    return r
+
+@app.route ("/form_add_pessoa")
+def form ():
+
+    return render_template ("add_pessoa.html")
 
 @app.route ("/danesse")
 def tralar ():
